837da3f7d7e1595b76e83fa380ffb03ed144a10d
ckf225 <ckf225@lehigh.edu>
2023-04-04 17:27:40 -0400

    front page done

--- app.py
+++ app.py
@@ -7,10 +7,16 @@ app = Flask(__name__)
 def home():
     conn = sqlite3.connect('maintextbooks.db')
     c = conn.cursor()
-    c.execute('SELECT * FROM textbooks')
-    textbooks = [{'isbn': row[0], 'title': row[1], 'course': row[2]} for row in c.fetchall()]
+    c.execute('SELECT title, cost FROM textbooks order by cost desc')
+    cost = [{'title': row[0], 'cost': row[1]} for row in c.fetchmany(10)]
+    c.execute("select title, count(*) as genderCount from (select title, gender from textbooks natural join authors where gender='female') group by title order by count() desc")
+    gender = [{'title': row[0], 'femaleNum': row[1]} for row in c.fetchmany(10)]
+    c.execute("select title, course from textbooks left outer join (select * from authors where gender = 'female') b on textbooks.isbn=b.isbn where authorFirst is null;")
+    onlyMale = [{'title': row[0], 'course': row[1]} for row in c.fetchmany(10)]
+    c.execute("select university, count(*) as uniCount from (select title, university from textbooks natural join authors) group by university order by count() desc")
+    university = [{'university': row[0], 'uniCount': row[1]} for row in c.fetchmany(10)]
     conn.close()
-    return render_template('home.html', textbooks=textbooks)
+    return render_template('home.html', cost=cost, gender = gender, university = university, onlyMale = onlyMale)
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
