3b0fcfb849d0b25527facf30f21e4b7a713e6211
ckf225 <ckf225@lehigh.edu>
2023-04-04 10:44:48 -0400

    links

--- app.py
+++ app.py
@@ -43,4 +43,23 @@ def searchClass():
             textbook_description = row[2]
             return render_template('textbook.html', textbook_id=textbook_id, textbook_title=textbook_title, textbook_description=textbook_description)
         else:
-            return 'Textbook not found!'
+            return render_template('notFound.html', textbook_id=textbook_id)
+
+@app.route('/textbook', methods=['GET', 'POST'])
+def click():
+    textbook_id = request.args.get('bookName', default = 'n/a', type = str)
+    print(textbook_id + "\n\n\n\n\n\n\n\n\n")
+    if textbook_id == 'n/a':
+        return render_template('notFound.html', textbook_id=textbook_id)
+    conn = sqlite3.connect('maintextbooks.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM textbooks WHERE textbook_title = ?', (textbook_id,))
+    row = c.fetchone()
+    conn.close()
+    if row is not None:
+        textbook_title = row[1]
+        textbook_description = row[2]
+        return render_template('textbook.html', textbook_id=textbook_id, textbook_title=textbook_title, textbook_description=textbook_description)
+    else:
+        return render_template('notFound.html', textbook_id=textbook_id)
+
